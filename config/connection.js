aba15f0125ec824cd174fa565a30b70e77407621
ajmalMundumuzhi <muhajmal73@gmail.com>
2025-05-19 06:50:42 +0530

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+async function connectDB () {
+    await mongoose.connect(process.env.MONGODB_URI, {
+        dbName: 'crudDB',
+    })
+
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed");
+    })
+}
+
+export default connectDB;
